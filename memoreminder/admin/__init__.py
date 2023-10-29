b941f6b4ebcc705a336263cbb4e4e50f58843eff
kianomoomi <kianomoomi1380@gmail.com>
2023-10-29 11:02:57 +0330

    feat: add comment admin page

--- memoreminder/admin/__init__.py
+++ memoreminder/admin/__init__.py
@@ -4,3 +4,5 @@ from .post_file import PostFileAdmin
 from .tag import TagAdmin
 from .memo_user import MemoUserAdmin
 from .friend_requests import FriendRequestAdmin
+from .comment import CommentAdmin
+from .comment_like import CommentLikeAdmin
